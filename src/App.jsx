3d85d146ea1876e36bc26aba2189335f81ca6f8b
RokonHardwere <rokonuzzamanmme@gmail.com>
2026-06-09 08:16:19 +0600

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Countries from './component/countries/Countries'
+
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+        <Countries></Countries>
+       <h1>React World Go One</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/countries/Countries.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h4>Wellcome to React world</h4>
+        </div>
+    );
+};
+
+export default Countries;
